764a2f039c9bdafcfdd9add86f7ef43a9d3db823
s1n7ax <srineshnisala@gmail.com>
2023-07-13 23:48:41 +0530

    refactor: add transparent background

---
+++ lua/user/highlights/init.lua
@@ -0,0 +1,9 @@
+return {
+  Normal = { bg = 'none' },
+  LineNr = { bg = 'none' },
+  Folded = { bg = 'none' },
+  NonText = { bg = 'none' },
+  SpecialKey = { bg = 'none' },
+  VertSplit = { bg = 'none' },
+  SignColumn = { bg = 'none' },
+}
