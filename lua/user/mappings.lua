e1ff39fdab1b402cb2a78d789788aa2564090e76
s1n7ax <srineshnisala@gmail.com>
2023-07-25 23:32:21 +0530

    fix: t_c-l will close the term pop instead clearing

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -3,3 +3,28 @@ require('user.mappings.normal')
 require('user.mappings.insert')
 require('user.mappings.telescope')
 require('user.mappings.terminal')
+
+return function(maps)
+  -- disable window navigation mappings
+  maps.n['<C-h>'] = nil
+  maps.n['<C-j>'] = nil
+  maps.n['<C-k>'] = nil
+  maps.n['<C-l>'] = nil
+
+  maps.t['<C-h>'] = nil
+  maps.t['<C-j>'] = nil
+  maps.t['<C-k>'] = nil
+  maps.t['<C-l>'] = nil
+
+  -- disable save & quite keymap
+  maps.n['<C-s>'] = nil
+  maps.n['<leader>w'] = nil
+
+  maps.n['<C-q>'] = nil
+  maps.n['<leader>q'] = nil
+
+  maps.n["<C-s>"] = { "<cmd>w<cr>", desc = "Save" }
+  maps.n["<C-q>"] = { "<cmd>confirm q<cr>", desc = "Quit" }
+
+  return maps
+end
