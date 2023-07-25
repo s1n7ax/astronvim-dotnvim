1de25e1831a53664c95cb2e17943005c5995be32
s1n7ax <srineshnisala@gmail.com>
2023-07-25 23:30:37 +0530

    fix: <c-i> has no colemak alternative

--- lua/user/mappings/colemak-remaps.lua
+++ lua/user/mappings/colemak-remaps.lua
@@ -30,6 +30,8 @@ wk.register({
 
 	j = { 'm', 'Create mark' },
 	J = { 'M', 'Middle line of window' },
+
+	['<c-l>'] = { '<c-i>', 'Jump to previous jump point' }
 }, {
 	mode = { 'n', 'x', 'o' },
 })
