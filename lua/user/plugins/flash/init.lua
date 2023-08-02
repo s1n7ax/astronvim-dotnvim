de437be92896847d7b15ca837859c11dd0a2421d
s1n7ax <srineshnisala@gmail.com>
2023-08-02 21:40:20 +0530

    chore(format): code format

--- lua/user/plugins/flash/init.lua
+++ lua/user/plugins/flash/init.lua
@@ -6,20 +6,20 @@ end
 
 local keys = {
 	{ 's', mode = { 'n', 'x', 'o' }, flash('jump'), desc = 'Flash' },
+	{ '<c-s>', mode = { 'c' }, flash('toggle'), desc = 'Toggle Flash Search' },
+	{ 'r', mode = 'o', flash('remote'), desc = 'Remote Flash' },
 	{
 		'S',
 		mode = { 'n', 'o', 'x' },
 		flash('treesitter'),
 		desc = 'Flash Treesitter',
 	},
-	{ 'r', mode = 'o', flash('remote'), desc = 'Remote Flash' },
 	{
 		'R',
 		mode = { 'o', 'x' },
 		flash('treesitter_search'),
 		desc = 'Treesitter Search',
 	},
-	{ '<c-s>', mode = { 'c' }, flash('toggle'), desc = 'Toggle Flash Search' },
 }
 
 return {
