dbde9a07676103e1c3b55e23e77d60d5bbebe3bd
s1n7ax <srineshnisala@gmail.com>
2023-08-03 23:18:47 +0530

    chore(format): code format

--- lua/user/plugins/mini-move/init.lua
+++ lua/user/plugins/mini-move/init.lua
@@ -1,21 +1,19 @@
 return {
-  'echasnovski/mini.move',
-  event = 'VeryLazy',
-  config = function()
-    require('mini.move').setup({
-      mappings = {
-        -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-        left = '<c-m>',
-        right = '<c-i>',
-        down = '<c-n>',
-        up = '<c-e>',
+	'echasnovski/mini.move',
+	event = 'VeryLazy',
+	config = function()
+		require('mini.move').setup({
+			mappings = {
+				left = '<c-m>',
+				right = '<c-i>',
+				down = '<c-n>',
+				up = '<c-e>',
 
-        -- Move current line in Normal mode
-        line_left = nil,
-        line_right = nil,
-        line_down = nil,
-        line_up = nil,
-      },
-    })
-  end,
+				line_left = nil,
+				line_right = nil,
+				line_down = nil,
+				line_up = nil,
+			},
+		})
+	end,
 }
