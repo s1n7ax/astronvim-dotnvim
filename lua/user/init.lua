f2ab430432d5bf286c82055dccc4b1d2361ec879
s1n7ax <srineshnisala@gmail.com>
2023-09-05 23:09:35 +0530

    feat: enable lua and rust LSPs

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,7 +1,13 @@
 return {
-  lsp = {
-    formatting = {
-      format_on_save = true,
-    },
-  },
+	colorscheme = 'catppuccin-mocha',
+
+	lsp = {
+		servers = {
+			'lua_ls',
+			'rust_analyzer',
+		},
+		formatting = {
+			format_on_save = true,
+		},
+	},
 }
