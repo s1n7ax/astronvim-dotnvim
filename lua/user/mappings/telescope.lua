170bfcf5c3601f380cbf58cc1cfadbd153a11300
s1n7ax <srineshnisala@gmail.com>
2023-09-05 23:08:09 +0530

    refactor: telescope to list all files

--- lua/user/mappings/telescope.lua
+++ lua/user/mappings/telescope.lua
@@ -1,6 +1,9 @@
 local wk = require('which-key')
 
 wk.register({
-  name = 'Telescope',
-  [',,'] = { '<cmd>Telescope find_files<cr>', 'Find Files' }
+	name = 'Telescope',
+	[',,'] = {
+		'<cmd>Telescope find_files find_command=rg,--no-ignore,--hidden,--files<cr>',
+		'Find Files',
+	},
 })
