167b31637919e94b6a8294710b71878bfeb6da86
s1n7ax <srineshnisala@gmail.com>
2023-09-05 23:10:15 +0530

    refactor: disable custom keymaps

--- lua/user/highlights/init.lua
+++ lua/user/highlights/init.lua
@@ -1,9 +1,9 @@
 return {
-  Normal = { bg = 'none' },
-  LineNr = { bg = 'none' },
-  Folded = { bg = 'none' },
-  NonText = { bg = 'none' },
-  SpecialKey = { bg = 'none' },
-  VertSplit = { bg = 'none' },
-  SignColumn = { bg = 'none' },
+  -- Normal = { bg = 'none' },
+  -- LineNr = { bg = 'none' },
+  -- Folded = { bg = 'none' },
+  -- NonText = { bg = 'none' },
+  -- SpecialKey = { bg = 'none' },
+  -- VertSplit = { bg = 'none' },
+  -- SignColumn = { bg = 'none' },
 }
