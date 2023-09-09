8c02336aa00f9c881b892e7c5474ef97cf099713
s1n7ax <srineshnisala@gmail.com>
2023-09-09 12:54:28 +0530

    fix: overlapping keymaps between cmp & luasnip

--- lua/user/plugins/cmp/init.lua
+++ lua/user/plugins/cmp/init.lua
@@ -1,10 +1,41 @@
-return {
-  'hrsh7th/nvim-cmp',
-  opts = function(_, opts)
-    opts.completion = {
-      completeopt = 'menu,menuone',
-    }
+local function has_words_before()
+	local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0
+		and vim.api
+				.nvim_buf_get_lines(0, line - 1, line, true)[1]
+				:sub(col, col)
+				:match('%s')
+			== nil
+end
 
-    return opts
-  end
+return {
+	'hrsh7th/nvim-cmp',
+	opts = function(_, opts)
+		local cmp = require('cmp')
+		local luasnip = require('luasnip')
+
+		opts.mapping = {
+			['<C-e>'] = cmp.mapping.select_prev_item({
+				behavior = cmp.SelectBehavior.Insert,
+			}),
+			['<C-n>'] = cmp.mapping.select_next_item({
+				behavior = cmp.SelectBehavior.Insert,
+			}),
+			['<C-u>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+			['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+			['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+			['<C-y>'] = cmp.config.disable,
+			['<C-l>'] = cmp.mapping({
+				i = cmp.mapping.abort(),
+				c = cmp.mapping.close(),
+			}),
+			['<CR>'] = cmp.mapping.confirm({ select = false }),
+		}
+
+		opts.completion = {
+			completeopt = 'menu,menuone',
+		}
+
+		return opts
+	end,
 }
--- lua/user/plugins/luasnip/init.lua
+++ lua/user/plugins/luasnip/init.lua
@@ -1,25 +1,28 @@
 local languages = {
-  'lua',
-  'javascript',
-  'javascriptreact',
-  'yaml',
-  'java',
-  'rust',
-  'bash'
+	'lua',
+	'javascript',
+	'javascriptreact',
+	'yaml',
+	'java',
+	'rust',
+	'bash',
 }
 
 return {
-  "L3MON4D3/LuaSnip",
-  dependencies = { 's1n7ax/nvim-snips', 'nvim-treesitter/nvim-treesitter', 's1n7ax/nvim-ts-utils' },
-  opts = function()
-    local helper = require('user.plugins.luasnip.helper')
+	'L3MON4D3/LuaSnip',
+	dependencies = {
+		's1n7ax/nvim-snips',
+		'nvim-treesitter/nvim-treesitter',
+		's1n7ax/nvim-ts-utils',
+	},
+	opts = function()
+		local helper = require('user.plugins.luasnip.helper')
 
+		-- Registering snippets
+		for _, language in pairs(languages) do
+			require(string.format('user.plugins.luasnip.snippets.%s', language)).setup()
+		end
 
-    -- Registering snippets
-    for _, language in pairs(languages) do
-      require(string.format('user.plugins.luasnip.snippets.%s', language)).setup()
-    end
-
-    helper.register_keymaps()
-  end
+		helper.register_keymaps()
+	end,
 }
