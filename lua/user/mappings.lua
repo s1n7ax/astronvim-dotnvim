da994c8a4cdbf6f90e7ef007e8cae9fcf39fdfe9
s1n7ax <srineshnisala@gmail.com>
2023-09-14 01:19:09 +0530

    chore: code format

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -5,26 +5,26 @@ require('user.mappings.telescope')
 require('user.mappings.terminal')
 
 return function(maps)
-  -- disable window navigation mappings
-  maps.n['<C-h>'] = nil
-  maps.n['<C-j>'] = nil
-  maps.n['<C-k>'] = nil
-  maps.n['<C-l>'] = nil
+	-- disable window navigation mappings
+	maps.n['<C-h>'] = nil
+	maps.n['<C-j>'] = nil
+	maps.n['<C-k>'] = nil
+	maps.n['<C-l>'] = nil
 
-  maps.t['<C-h>'] = nil
-  maps.t['<C-j>'] = nil
-  maps.t['<C-k>'] = nil
-  maps.t['<C-l>'] = nil
+	maps.t['<C-h>'] = nil
+	maps.t['<C-j>'] = nil
+	maps.t['<C-k>'] = nil
+	maps.t['<C-l>'] = nil
 
-  -- disable save & quite keymap
-  maps.n['<C-s>'] = nil
-  maps.n['<leader>w'] = nil
+	-- disable save & quite keymap
+	maps.n['<C-s>'] = nil
+	maps.n['<leader>w'] = nil
 
-  maps.n['<C-q>'] = nil
-  maps.n['<leader>q'] = nil
+	maps.n['<C-q>'] = nil
+	maps.n['<leader>q'] = nil
 
-  maps.n["<C-s>"] = { "<cmd>w<cr>", desc = "Save" }
-  maps.n["<C-q>"] = { "<cmd>confirm q<cr>", desc = "Quit" }
+	maps.n['<C-s>'] = { '<cmd>w<cr>', desc = 'Save' }
+	maps.n['<C-q>'] = { '<cmd>confirm q<cr>', desc = 'Quit' }
 
-  return maps
+	return maps
 end
