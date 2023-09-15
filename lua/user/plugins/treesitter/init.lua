9aa793334b3eff988ef365869a20d23f0c6a9738
s1n7ax <srineshnisala@gmail.com>
2023-09-15 23:41:27 +0530

    feat: add ensure ts installed list

--- lua/user/plugins/treesitter/init.lua
+++ lua/user/plugins/treesitter/init.lua
@@ -1,27 +1,71 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = ",n",
-        node_incremental = ",n",
-        scope_incremental = ",s",
-        node_decremental = ",e",
-      },
-    },
+	'nvim-treesitter/nvim-treesitter',
+	opts = {
+		ensure_installed = {
+			'bash',
+			'c',
+			'cmake',
+			'comment',
+			'cpp',
+			'css',
+			'csv',
+			'diff',
+			'dockerfile',
+			'gitcommit',
+			'gitignore',
+			'html',
+			'java',
+			'javascript',
+			'jsdoc',
+			'json5',
+			'lua',
+			'markdown_inline',
+			'nix',
+			'python',
+			'query',
+			'regex',
+			'requirements',
+			'rust',
+			'scss',
+			'sql',
+			'todotxt',
+			'tsx',
+			'typescript',
+			'vimdoc',
+			'vue',
+			'xml',
+			'yaml',
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = ',n',
+				node_incremental = ',n',
+				scope_incremental = ',s',
+				node_decremental = ',e',
+			},
+		},
 
-    text_object           = {
-      select = {
-        keymaps = {
-          ["kk"] = { query = "@block.inner", desc = "inside block" },
-          ["kc"] = { query = "@class.inner", desc = "inside class" },
-          ["k?"] = { query = "@conditional.inner", desc = "inside conditional" },
-          ["kf"] = { query = "@function.inner", desc = "inside function " },
-          ["kl"] = { query = "@loop.inner", desc = "inside loop" },
-          ["ka"] = { query = "@parameter.inner", desc = "inside argument" },
-        },
-      }
-    }
-  }
+		text_object = {
+			select = {
+				keymaps = {
+					['kk'] = { query = '@block.inner', desc = 'inside block' },
+					['kc'] = { query = '@class.inner', desc = 'inside class' },
+					['k?'] = {
+						query = '@conditional.inner',
+						desc = 'inside conditional',
+					},
+					['kf'] = {
+						query = '@function.inner',
+						desc = 'inside function ',
+					},
+					['kl'] = { query = '@loop.inner', desc = 'inside loop' },
+					['ka'] = {
+						query = '@parameter.inner',
+						desc = 'inside argument',
+					},
+				},
+			},
+		},
+	},
 }
