e84c6024315a4e8dc68e71e98f7d7cc02695774d
s1n7ax <srineshnisala@gmail.com>
2023-09-16 01:47:14 +0530

    chore: code format

--- lua/user/mappings/normal.lua
+++ lua/user/mappings/normal.lua
@@ -5,45 +5,45 @@ local wk = require('which-key')
 --                             Windows                              --
 ----------------------------------------------------------------------
 wk.register({
-  name = 'Windows',
+	name = 'Windows',
 
-  ['<c-w><c-w>'] = { '<c-w>p', 'Jump to last window' },
-  ['<c-m>'] = { '<c-w>h', 'Jump to left window' },
-  ['<c-i>'] = { '<c-w>l', 'Jump to right window' },
-  ['<c-n>'] = { '<c-w>j', 'Jump to below window' },
-  ['<c-e>'] = { '<c-w>k', 'Jump to above window' },
+	['<c-w><c-w>'] = { '<c-w>p', 'Jump to last window' },
+	['<c-m>'] = { '<c-w>h', 'Jump to left window' },
+	['<c-i>'] = { '<c-w>l', 'Jump to right window' },
+	['<c-n>'] = { '<c-w>j', 'Jump to below window' },
+	['<c-e>'] = { '<c-w>k', 'Jump to above window' },
 })
 
 wk.register({
-  name = 'Split window',
-  m = { window_util.split_left, 'Split left' },
-  n = { window_util.split_bottom, 'Split bottom' },
-  e = { window_util.split_top, 'Split top' },
-  i = { window_util.split_right, 'Split right' },
+	name = 'Split window',
+	m = { window_util.split_left, 'Split left' },
+	n = { window_util.split_bottom, 'Split bottom' },
+	e = { window_util.split_top, 'Split top' },
+	i = { window_util.split_right, 'Split right' },
 }, {
-  prefix = '<tab>',
+	prefix = '<tab>',
 })
 ----------------------------------------------------------------------
 --                             Editing                              --
 ----------------------------------------------------------------------
 wk.register({
-  ['[<space>'] = {
-    function()
-      local curr_line = vim.api.nvim_win_get_cursor(0)[1]
-      local prev_line = curr_line - 1
-      vim.api.nvim_buf_set_lines(0, prev_line, prev_line, true, { '' })
-      vim.api.nvim_input('<up>')
-    end,
-    'Add line above',
-  },
-  [']<space>'] = {
-    function()
-      local curr_line = vim.api.nvim_win_get_cursor(0)[1]
-      vim.api.nvim_buf_set_lines(0, curr_line, curr_line, true, { '' })
-      vim.api.nvim_input('<down>')
-    end,
-    'Add line below',
-  },
+	['[<space>'] = {
+		function()
+			local curr_line = vim.api.nvim_win_get_cursor(0)[1]
+			local prev_line = curr_line - 1
+			vim.api.nvim_buf_set_lines(0, prev_line, prev_line, true, { '' })
+			vim.api.nvim_input('<up>')
+		end,
+		'Add line above',
+	},
+	[']<space>'] = {
+		function()
+			local curr_line = vim.api.nvim_win_get_cursor(0)[1]
+			vim.api.nvim_buf_set_lines(0, curr_line, curr_line, true, { '' })
+			vim.api.nvim_input('<down>')
+		end,
+		'Add line below',
+	},
 })
 
 ----------------------------------------------------------------------
@@ -52,10 +52,13 @@ wk.register({
 local is_inlay_hint_active = false
 
 wk.register({
-  a = { function()
-    is_inlay_hint_active = not (is_inlay_hint_active)
-    vim.lsp.buf.inlay_hint(0, is_inlay_hint_active)
-  end, 'Toggle Inlay Hint' }
+	a = {
+		function()
+			is_inlay_hint_active = not is_inlay_hint_active
+			vim.lsp.buf.inlay_hint(0, is_inlay_hint_active)
+		end,
+		'Toggle Inlay Hint',
+	},
 }, {
-  prefix = '<leader><leader>'
+	prefix = '<leader><leader>',
 })
