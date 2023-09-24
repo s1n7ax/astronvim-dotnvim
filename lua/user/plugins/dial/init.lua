497bbbc47a624e9713448bcf2b65826d872d379a
s1n7ax <srineshnisala@gmail.com>
2023-09-24 06:01:36 +0530

    feat: add lua and java specific augends

--- lua/user/plugins/dial/init.lua
+++ lua/user/plugins/dial/init.lua
@@ -1,59 +1,109 @@
-local function inc()
-	require('dial.map').manipulate('increment', 'normal')
+local function manupilate(direction, mode, group)
+	return function()
+		require('dial.map').manipulate(direction, mode, group)
+	end
 end
 
-local function dec()
-	require('dial.map').manipulate('decrement', 'normal')
-end
-
-local function inc_g()
-	require('dial.map').manipulate('increment', 'gnormal')
-end
-
-local function dec_g()
-	require('dial.map').manipulate('decrement', 'gnormal')
-end
-
-local function inc_v()
-	require('dial.map').manipulate('increment', 'visual')
-end
-
-local function dec_v()
-	require('dial.map').manipulate('decrement', 'visual')
-end
-
-local function inc_gv()
-	require('dial.map').manipulate('increment', 'gvisual')
-end
-
-local function dec_gv()
-	require('dial.map').manipulate('decrement', 'gvisual')
+local function concat_table(t1, t2)
+	for i = 1, #t2 do
+		t1[#t1 + 1] = t2[i]
+	end
+	return t1
 end
 
 return {
 	'monaqa/dial.nvim',
+	event = 'VeryLazy',
 	keys = {
-		{ '<C-a>', inc, desc = 'Increment', mode = 'n' },
-		{ '<c-x>', dec, desc = 'Decrement', mode = 'n' },
-		{ 'g<C-a>', inc_g, desc = 'Increment', mode = 'n' },
-		{ 'g<C-x>', dec_g, desc = 'Decrement', mode = 'n' },
+		{
+			'<C-a>',
+			manupilate('increment', 'normal'),
+			desc = 'Increment',
+			mode = 'n',
+		},
+		{
+			'<c-x>',
+			manupilate('decrement', 'normal'),
+			desc = 'Decrement',
+			mode = 'n',
+		},
+		{
+			'g<C-a>',
+			manupilate('increment', 'gnormal'),
+			desc = 'Increment',
+			mode = 'n',
+		},
+		{
+			'g<C-x>',
+			manupilate('decrement', 'gnormal'),
+			desc = 'Decrement',
+			mode = 'n',
+		},
 
-		{ '<C-a>', inc_v, desc = 'Visual Increment', mode = 'v' },
-		{ '<C-x>', dec_v, desc = 'Visual Decrement', mode = 'v' },
-		{ 'g<C-a>', inc_gv, desc = 'Visual Increment', mode = 'v' },
-		{ 'g<C-x>', dec_gv, desc = 'Visual Decrement', mode = 'v' },
+		{
+			'<C-a>',
+			manupilate('increment', 'visual'),
+			desc = 'Visual Increment',
+			mode = 'v',
+		},
+		{
+			'<C-x>',
+			manupilate('decrement', 'visual'),
+			desc = 'Visual Decrement',
+			mode = 'v',
+		},
+		{
+			'g<C-a>',
+			manupilate('increment', 'gvisual'),
+			desc = 'Visual Increment',
+			mode = 'v',
+		},
+		{
+			'g<C-x>',
+			manupilate('decrement', 'gvisual'),
+			desc = 'Visual Decrement',
+			mode = 'v',
+		},
 	},
 	config = function()
 		local augend = require('dial.augend')
 
+		local common = {
+			augend.integer.alias.decimal,
+			augend.integer.alias.hex,
+			augend.constant.alias.bool,
+			augend.date.alias['%Y/%m/%d'],
+			augend.date.alias['%d/%m/%Y'],
+		}
+
+		local lua = concat_table(common, {
+			augend.constant.new({
+				elements = { 'and', 'or' },
+				word = true,
+				cyclic = true,
+			}),
+		})
+
+		local java = concat_table(common, {
+			augend.constant.new({
+				elements = { 'private', 'public', 'protected' },
+				word = true,
+				cyclic = true,
+			}),
+			augend.constant.new({
+				elements = { 'class', 'interface', 'enum' },
+				word = true,
+				cyclic = true,
+			}),
+		})
+
 		require('dial.config').augends:register_group({
-			default = {
-				augend.integer.alias.decimal,
-				augend.integer.alias.hex,
-				augend.constant.alias.bool,
-				augend.date.alias['%Y/%m/%d'],
-				augend.date.alias['%d/%m/%Y'],
-			},
+			default = common,
+		})
+
+		require('dial.config').augends:on_filetype({
+			lua,
+			java = java,
 		})
 	end,
 }
