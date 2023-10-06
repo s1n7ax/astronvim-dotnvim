2c8450a98c54e978ccf3674988d17689b8e1980a
s1n7ax <srineshnisala@gmail.com>
2023-10-06 10:26:24 +0530

    refactor: switch from neo-tree side bar to float

--- lua/user/plugins/neo-tree/init.lua
+++ lua/user/plugins/neo-tree/init.lua
@@ -2,6 +2,8 @@ return {
 	'nvim-neo-tree/neo-tree.nvim',
 	opts = function(_, neotree_conf)
 		neotree_conf.window.mappings.e = 'none'
+		neotree_conf.window.position = 'float'
+
 		return neotree_conf
 	end,
 }
