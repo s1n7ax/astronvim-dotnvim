711119145cf0b49ce7de41443394d3012e08a2f6
s1n7ax <srineshnisala@gmail.com>
2023-10-10 19:47:30 +0530

    chore: code clean up

--- lua/user/plugins/cmp/init.lua
+++ lua/user/plugins/cmp/init.lua
@@ -1,18 +1,7 @@
-local function has_words_before()
-	local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0
-		and vim.api
-				.nvim_buf_get_lines(0, line - 1, line, true)[1]
-				:sub(col, col)
-				:match('%s')
-			== nil
-end
-
 return {
 	'hrsh7th/nvim-cmp',
 	opts = function(_, opts)
 		local cmp = require('cmp')
-		local luasnip = require('luasnip')
 
 		opts.mapping = {
 			['<C-e>'] = cmp.mapping.select_prev_item({
