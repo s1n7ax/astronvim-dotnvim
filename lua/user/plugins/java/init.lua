e544428074730554db22e56432f76f349fac652d
s1n7ax <srineshnisala@gmail.com>
2023-11-06 12:09:51 +0530

    fix: add nvim-java required dependencies

--- lua/user/plugins/java/init.lua
+++ lua/user/plugins/java/init.lua
@@ -2,6 +2,11 @@ local file = require('user.utils.file')
 
 return {
 	's1n7ax/nvim-java',
+	dependencies = {
+		'neovim/nvim-lspconfig',
+		'williamboman/mason.nvim',
+		'mfussenegger/nvim-dap',
+	},
 	event = 'VeryLazy',
 	dir = file.is_pkg_dir('~/Workspace/nvim-java'),
 }
