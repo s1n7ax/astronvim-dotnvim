02489b1cefbc83a147f9dfd2821b1c0e5fa42ce0
s1n7ax <srineshnisala@gmail.com>
2023-11-13 16:47:29 +0530

    refactor(plug): nvim-java spec update to new core update

--- lua/user/plugins/java/init.lua
+++ lua/user/plugins/java/init.lua
@@ -1,12 +1,17 @@
 local file = require('user.utils.file')
 
 return {
-	's1n7ax/nvim-java',
+	'nvim-java/nvim-java',
 	dependencies = {
+		{
+			'nvim-java/nvim-java-core',
+			dir = file.is_pkg_dir('~/Workspace/nvim-java-core'),
+		},
 		'neovim/nvim-lspconfig',
 		'williamboman/mason.nvim',
 		'mfussenegger/nvim-dap',
 	},
 	event = 'VeryLazy',
 	dir = file.is_pkg_dir('~/Workspace/nvim-java'),
+	opts = {},
 }
