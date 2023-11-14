855c45a2d2900320ef674d2fce40ace64350aa36
s1n7ax <srineshnisala@gmail.com>
2023-11-14 18:44:30 +0530

    fix: use new mason registry at nvim-java org

--- lua/user/plugins/mason/init.lua
+++ lua/user/plugins/mason/init.lua
@@ -2,7 +2,7 @@ return {
 	'williamboman/mason.nvim',
 	opts = function(_, opts)
 		opts.registries = {
-			'github:s1n7ax/mason-registry',
+			'github:nvim-java/nvim-java-mason-registry',
 			'github:mason-org/mason-registry',
 		}
 
