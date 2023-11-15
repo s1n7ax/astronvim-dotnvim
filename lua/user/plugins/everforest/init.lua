01da2d3ab96e985d4e4cc88da68c56c8dce0fb6b
s1n7ax <srineshnisala@gmail.com>
2023-11-16 00:29:12 +0530

    chore(ui): make the theme transparent

--- lua/user/plugins/everforest/init.lua
+++ lua/user/plugins/everforest/init.lua
@@ -3,6 +3,8 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require('everforest').setup({})
+		require('everforest').setup({
+			transparent_background_level = 2,
+		})
 	end,
 }
