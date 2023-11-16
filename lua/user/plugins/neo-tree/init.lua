d325b01ae4abfed4a1fb1ac8ab768841fd90cb56
s1n7ax <srineshnisala@gmail.com>
2023-11-16 10:21:06 +0530

    refactor: neo-tree position to current

--- lua/user/plugins/neo-tree/init.lua
+++ lua/user/plugins/neo-tree/init.lua
@@ -2,7 +2,7 @@ return {
 	'nvim-neo-tree/neo-tree.nvim',
 	opts = function(_, neotree_conf)
 		neotree_conf.window.mappings.e = 'none'
-		neotree_conf.window.position = 'float'
+		neotree_conf.window.position = 'current'
 
 		return neotree_conf
 	end,
