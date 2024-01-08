e52a20fe43a6c63bcc978ba586b0939540e49bef
s1n7ax <srineshnisala@gmail.com>
2024-01-08 22:24:57 +0530

    feat: add lua postfix snips

--- lua/user/plugins/luasnip/snippets/lua.lua
+++ lua/user/plugins/luasnip/snippets/lua.lua
@@ -6,25 +6,26 @@ function M.setup()
 	local common = require('snips.common')
 	local s = ls.s
 	local i = ls.insert_node
+	local postfix = require('luasnip.extras.postfix').postfix
 
 	ls.add_snippets('lua', {
-		s('f', lua.choices.func(), {
-			stored = {
-				name = i(1, 'name'),
-			},
-		}),
-		s('fd', lua.choices.func_with_doc(), {
-			stored = {
-				name = i(1, 'name'),
-			},
-		}),
-		s('mod', lua.choices.module(), {
-			stored = {
-				name = i(1, 'M'),
-			},
-		}),
+		-- postfix
+		postfix({
+			trig = '.v',
+			match_pattern = '[%w%.%_%-%(%)]+$',
+		}, lua.postfix.variable()),
+		postfix({
+			trig = '.r',
+			match_pattern = '[%w%.%_%-%(%)]+$',
+		}, lua.postfix.returns()),
+
+		-- choice & dynamic
+		s('f', lua.choices.func()),
+		s('fd', lua.choices.func_with_doc()),
+		s('mod', lua.choices.module()),
 		s('v', lua.dynamic.variable()),
 
+		-- primitives
 		s('faa', lua.primitives.anonymous_func()),
 		s('fa', lua.primitives.noarg_anonymous_func()),
 		s('i', lua.primitives.import()),
